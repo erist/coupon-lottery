19f3d7592dc17cad8d0fc0999ef3ad82ca569a0d
erist <cucuyh@gmail.com>
2021-11-10 14:19:10 +0900

    Add: firebase/firestore

---
+++ src/services/firestore.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+firebase.initializeApp({
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID
+});
+const db = firebase.firestore();
+
+export default db;
