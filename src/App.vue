daf78e07275ca27ee178d121d2eec2bc63b7b5a8
erist <cucuyh@gmail.com>
2021-11-10 16:39:26 +0900

    Add: login feature

--- src/App.vue
+++ src/App.vue
@@ -5,56 +5,207 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
       <v-spacer></v-spacer>
-
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
+        v-show="isSignedIn"
         text
+        @click="() => { this.showMyCouponDialog = true; }"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2">내 쿠폰 보기</span>
       </v-btn>
+      <v-btn
+        text
+        @click="isSignedIn ? onClickLogout() : onClickLogin()"
+      >
+        <span class="mr-2">{{ isSignedIn ? "로그아웃" : "로그인" }}</span>
+      </v-btn>
+      <v-dialog
+          v-model="showLoginDialog"
+          max-width="500px"
+      >
+        <v-card>
+          <v-card-title>
+            <span class="text-h5">로그인/등록</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-text-field
+                  v-model="username"
+                  label="게임 내 닉네임"
+              ></v-text-field>
+              <v-text-field
+                  v-model="password"
+                  label="쿠폰 확인용 암호"
+                  type="password"
+                  hint="* 절대로 사용중인 비밀번호 넣지 마세요!"
+              ></v-text-field>
+            </v-container>
+            <v-card-actions>
+              <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="onClickRegister()"
+              >
+                등록
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="onClickClose()"
+              >
+                취소
+              </v-btn>
+              <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="onClickSignIn()"
+              >
+                로그인
+              </v-btn>
+            </v-card-actions>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+      <v-dialog
+        v-model="showMyCouponDialog"
+        scrollable
+        max-width="500px"
+      >
+        <v-card>
+          <v-card-title>
+            <span class="text-h5">내 쿠폰</span>
+          </v-card-title>
+          <v-card-text>
+            <v-list>
+              <p v-if="userData.coupons.length === 0">쿠폰이 없습니다.</p>
+              <v-list-item
+                v-for="coupon in userData.coupons"
+                :key="coupon.code">
+                <v-list-item-content>
+                  <v-list-item-title v-text="coupon.name"></v-list-item-title>
+                  <v-list-item-title v-text="coupon.code"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="() => { this.showMyCouponDialog = false; }"
+              >
+                닫기
+              </v-btn>
+            </v-card-actions>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
     </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
+      <Home/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Home from '@/views/Home';
+import firestore from '@/services/firestore';
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'App',
-
   components: {
-    HelloWorld,
+    Home,
   },
-
   data: () => ({
-    //
+    showLoginDialog: false,
+    username: '',
+    password: '',
+    showMyCouponDialog: false,
   }),
+  computed: {
+    ...mapGetters([
+      'isSignedIn',
+      'userData'
+    ]),
+  },
+  methods: {
+    ...mapActions([
+      'signIn',
+      'signOut',
+    ]),
+    onClickMyCoupons: function () {
+
+    },
+    onClickLogin: function () {
+      this.showLoginDialog = true;
+    },
+    onClickLogout: function () {
+      this.signOut();
+    },
+    /* LoginDialog */
+    getUserByUsername: async function (username) {
+      const querySnapshot = await firestore.collection('Users')
+          .where('username', '==', username)
+          .get();
+      if (querySnapshot.docs.length > 0) {
+        return querySnapshot.docs[0];
+      }
+      return null;
+    },
+    onClickSignIn: async function () {
+      const userDoc = await this.getUserByUsername(this.username);
+      if (!userDoc) {
+        alert("등록되지 않은 사용자입니다. 등록부터 하셔야 합니다.");
+        return;
+      }
+      const { password } = userDoc.data();
+      if (password !== this.password) {
+        alert("확인코드가 맞지 않습니다. 다시 한번 확인해 주세요.");
+        return;
+      }
+      this.signIn({
+        id: userDoc.id,
+        username: userDoc.data().username,
+        coupons: userDoc.data().coupons
+      });
+      this.onClickClose();
+    },
+    onClickClose: function () {
+      this.showLoginDialog = false;
+    },
+    validateUsername: async function (username) {
+      const querySnapshot = await firestore.collection('ValidateUsers')
+          .where('username', '==', username)
+          .get();
+      return querySnapshot.docs.length > 0;
+    },
+    onClickRegister: async function () {
+      const userDoc = await this.getUserByUsername(this.username);
+      if (userDoc && userDoc.exists) {
+        alert("이미 등록되었습니다. 확인코드를 잊으신 경우 관리자에게 문의하세요.");
+        return;
+      }
+      const validate = await this.validateUsername(this.username);
+      if (!validate) {
+        alert("삼별초 동맹원만 참여가 가능합니다. 동맹원일 경우 관리자에게 문의해 주세요.");
+        return;
+      }
+      await firestore.collection('Users')
+        .add({
+          username: this.username,
+          password: this.password,
+          coupons: [],
+        });
+      const newUserDoc = await this.getUserByUsername(this.username);
+      this.signIn({
+        id: newUserDoc.id,
+        username: newUserDoc.data().username,
+        coupons: newUserDoc.data().coupons
+      });
+      this.onClickClose();
+    }
+  }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,45 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {}
+  state: {
+    signedIn: false,
+    id: '',
+    username: '',
+    coupons: [],
+  },
+  getters: {
+    isSignedIn: function (state) {
+      return state.signedIn;
+    },
+    userData: function (state) {
+      return {
+        id: state.id,
+        username: state.username,
+        coupons: state.coupons
+      };
+    }
+  },
+  mutations: {
+    setSignedIn: function (state, payload) {
+      state.id = payload.id;
+      state.username = payload.username;
+      state.coupons = payload.coupons;
+      state.signedIn = true;
+    },
+    setSignedOut: function (state, /* payload */) {
+      state.id = '';
+      state.username = '';
+      state.coupons = [];
+      state.signedIn = false
+    },
+
+  },
+  actions: {
+    signIn: function ({ commit }, payload) {
+      return commit('setSignedIn', payload);
+    },
+    signOut: function ({ commit }) {
+      return commit('setSignedOut');
+    }
+  }
 })
